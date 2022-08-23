81e25afcf360707af543cfc6275ff591b0bd168b
Sabina Hult <sahu@dbc.dk>
2022-08-23 13:03:30 +0200

    expressions, attributes, components, modules, imports, styling

---
+++ keeper-app/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+    <h1 contentEditable="true">Title</h1>
+    <p contentEditable="true">Write your note here</p>
+    </div>
+  );
+}
+
+export default Note;
